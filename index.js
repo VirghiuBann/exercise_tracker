a1b6acda7c74a018e2d50ee4824fce2d46e5b5fd
virghiu <virghiu@gmail.com>
2023-08-05 11:04:05 -0400

    feat: DB connection

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose');
 require('dotenv').config()
+
+const mongoUri = process.env.MONGO_URI;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const User = require('./model/User');
+const Exercise = require('./model/Exercise');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -12,7 +23,15 @@ app.get('/', (req, res) => {
 
 
 
-
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.error(console, 'MondoDB connection error:', error);
+});
+db.once('open',  async () => {
+  console.log('Connected to MongoDB successfully!');
+  await Exercise.deleteMany({});
+  await User.deleteMany({});
+})
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
