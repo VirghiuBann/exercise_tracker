495e3b24104e890295d481e4dbc0a0fdb5b644cf
virghiu <virghiu@gmail.com>
2023-08-05 11:09:05 -0400

    feat: create relationship User and Exercise

--- model/User.js
+++ model/User.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
   username:{
     type: String,
     required: true
-  }
+  },
+  exercises: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exercise'}],
 });
 
 module.exports = mongoose.model("User", userSchema);
